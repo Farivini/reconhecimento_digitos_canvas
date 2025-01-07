abe82326769d20c18a248321a3c428aa1e703352
Vinicius farineli freire <64615200+Farivini@users.noreply.github.com>
2025-01-06 21:14:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -286,6 +286,10 @@ O app converte seu desenho para 28x28, inverte as cores (para combinar com o MNI
 Se quiser apagar e tentar outro desenho, clique em "Resetar Canvas".
 """)
 
+# Resetar o canvas (com controle de estado)
+if "canvas_data" not in st.session_state:
+    st.session_state["canvas_data"] = None
+
 if st.button("Resetar Canvas"):
     st.session_state["canvas_digit"] = None
     st.session_state["canvas_reset"] = True  # Flag para resetar o canvas
